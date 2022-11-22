e1158ceed97ac4971a03c51998a251a98f98855c
mohanv2030 <mnocoder@gmail.com>
2022-11-22 19:40:41 +0530

    login page creation success

--- __init__.py
+++ __init__.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect
 #from gevent.pywsgi import WSGIServer
 from config import Config
 
 app = Flask(__name__,  template_folder='template')
 app.config.from_object(Config)
 
-#from app import routes
+import forms
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     # Serve the app with gevent
--- routes.py
+++ routes.py
@@ -1,12 +1,10 @@
 from flask import render_template, flash, redirect
-from app import app
-from app import forms
+import forms
 
-@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = forms.LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', form=form)
